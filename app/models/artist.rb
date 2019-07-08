aaf83b20f5f2a646fdc53b885e0c8bb4ff61b680
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-08 14:10:52 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,13 @@
 class Artist < ActiveRecord::Base
-  # add associations here
+  has_many :songs
+  has_many :genres, through: :songs
+  has_many :notes, through: :songs
+
+  def artist_name=(name)
+     self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
